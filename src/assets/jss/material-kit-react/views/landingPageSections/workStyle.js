413008328df2ce3442cd7341b9f4c4886a75fad7
polishponcho <polishponcho@gmail.com>
2020-08-17 21:26:17 -0500

    first commit

---
+++ src/assets/jss/material-kit-react/views/landingPageSections/workStyle.js
@@ -0,0 +1,32 @@
+import { title } from "assets/jss/material-kit-react.js";
+
+const workStyle = {
+  section: {
+    padding: "70px 0"
+  },
+  title: {
+    ...title,
+    marginBottom: "50px",
+    marginTop: "30px",
+    minHeight: "32px",
+    textDecoration: "none",
+    textAlign: "center"
+  },
+  description: {
+    color: "#999",
+    textAlign: "center"
+  },
+  textCenter: {
+    textAlign: "center"
+  },
+  textArea: {
+    marginRight: "15px",
+    marginLeft: "15px"
+  },
+  button: {
+    backgroundColor: "#FA8072",
+    color: "white"
+  }
+};
+
+export default workStyle;
---
+++ src/views/Components/Sections/SectionCarousel.js
@@ -0,0 +1,74 @@
+import React from "react";
+// react component for creating beautiful carousel
+import Carousel from "react-slick";
+// @material-ui/core components
+import { makeStyles } from "@material-ui/core/styles";
+// @material-ui/icons
+import LocationOn from "@material-ui/icons/LocationOn";
+// core components
+import GridContainer from "components/Grid/GridContainer.js";
+import GridItem from "components/Grid/GridItem.js";
+import Card from "components/Card/Card.js";
+
+import image1 from "assets/img/bg.jpg";
+import image2 from "assets/img/bg2.jpg";
+import image3 from "assets/img/bg3.jpg";
+
+import caregiverDay from "assets/img/caregiver-day.jpeg"
+import homeHealthCare from "assets/img/HomeHealth.jpeg"
+import HomeHealth from "assets/img/homeHealthCare.jpeg"
+import FatherandSons from "assets/img/FatherandSons.jpeg"
+import homeHealthCare2 from "assets/img/homeHealthCare2.jpeg"
+
+
+import styles from "assets/jss/material-kit-react/views/componentsSections/carouselStyle.js";
+
+const useStyles = makeStyles(styles);
+
+export default function SectionCarousel() {
+  const classes = useStyles();
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: false
+  };
+  return (
+    <div className={classes.section}>
+      <div className={classes.container}>
+        <GridContainer>
+          <GridItem xs={12} sm={12} md={8} className={classes.marginAuto}>
+            <Card carousel>
+              <Carousel {...settings}>
+                <div>
+                  <img src={caregiverDay} alt="First slide" className="slick-image" />
+                  <div className="slick-caption">
+             
+                  </div>
+                </div>
+                <div>
+                  <img
+                    src={FatherandSons}
+                    alt="Second slide"
+                    className="slick-image"
+                  />
+                  <div className="slick-caption">
+                  
+                  </div>
+                </div>
+                <div>
+                  <img src={homeHealthCare2} alt="Third slide" className="slick-image" />
+                  <div className="slick-caption">
+                    
+                  </div>
+                </div>
+              </Carousel>
+            </Card>
+          </GridItem>
+        </GridContainer>
+      </div>
+    </div>
+  );
+}
